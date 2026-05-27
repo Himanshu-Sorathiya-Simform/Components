499eb23636afa70d9ee3671499982b7f217ed3c4
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-27 15:55:46 +0530

    replace normal component with forward ref component to support older react versions

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,31 +1,33 @@
-import { type RefObject } from "react";
+import { forwardRef } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.css";
 
-export default function Modal({
-	ref,
-	onClose,
-	children,
-	className = "",
-}: {
-	ref: RefObject<HTMLDialogElement | null>;
+interface ModalProps {
 	onClose: () => void;
 	children: React.ReactNode;
 	className?: string;
-}) {
-	const handleCancel = (event: React.SyntheticEvent<HTMLDialogElement>) => {
-		event.preventDefault();
-		onClose();
-	};
-
-	return createPortal(
-		<dialog
-			ref={ref}
-			onCancel={handleCancel}
-			className={`universal-modal ${className}`}
-		>
-			<div className="modal-container">{children}</div>
-		</dialog>,
-		document.body,
-	);
 }
+
+const Modal = forwardRef<HTMLDialogElement, ModalProps>(
+	({ onClose, children, className = "" }, ref) => {
+		const handleCancel = (event: React.SyntheticEvent<HTMLDialogElement>) => {
+			event.preventDefault();
+			onClose();
+		};
+
+		return createPortal(
+			<dialog
+				ref={ref}
+				onCancel={handleCancel}
+				className={`universal-modal ${className}`}
+			>
+				<div className="modal-container">{children}</div>
+			</dialog>,
+			document.body,
+		);
+	},
+);
+
+Modal.displayName = "Modal";
+
+export default Modal;
