0829748f33d3f83d581f6fc64afdf6bb3ee9629a
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-27 16:52:23 +0530

    fix incorrect style for backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { type RefObject, useEffect, useRef } from "react";
+import Modal from "./Modal/Modal.tsx";
+
 function App() {
-	return null;
+	const ref: RefObject<HTMLDialogElement | null> = useRef(null);
+
+	useEffect(() => {
+		if (!ref.current) return;
+
+		ref.current.showModal();
+	});
+
+	return (
+		<div>
+			hello{" "}
+			<Modal
+				ref={ref}
+				onClose={() => {
+					if (!ref.current) return;
+
+					ref.current.close();
+				}}
+			>
+				<div>Hello</div>
+			</Modal>
+		</div>
+	);
 }
 
 export default App;
